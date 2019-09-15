f171c17a0b29e3c5f252dd2d4048f4f205738f3c
Dylan Stankievech <dylan.stank@gmail.com>
2019-09-15 16:30:54 -0600

    Find it() blocks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 
 const isTestFile = (fileName) => {
     return fileName && fileName.endsWith('.test.js') || fileName.endsWith('.spec.js');
@@ -12,7 +13,7 @@ const isDirectory = (path) => {
 
 const getTestFiles = (directory) => {
     const files = fs.readdirSync(directory);
-    let testFiles = files.filter(f => isTestFile(f));
+    let testFiles = files.filter(f => isTestFile(f)).map(f => path.join(directory, f));
     const directories = files.filter(f => isDirectory(f));
     directories.forEach(dir => {
         const subFiles = getTestFiles(dir);
@@ -22,14 +23,44 @@ const getTestFiles = (directory) => {
     return testFiles;
 };
 
+const isTestLine = (line) => {
+    const regex = /^\s*it\(/;
+    return regex.test(line);
+};
+
+const isTestBlock = (line) => {
+    testBlockRegex.match(line);
+}
+
+const findLargeTests = (testFile) => {
+    const testText = fs.readFileSync(testFile).toString();
+    if (!testText) {
+        return;
+    }
+
+    const lines = testText.split('\n');
+    if (!lines || !lines.length) {
+        return;
+    }
+
+    const numLines = lines.length;
+    for (let i = 0; i < numLines; i++) {
+        const line = lines[i];
+        const lineNumber = i + 1;
+        if (isTestLine(line)) {
+            console.log(`${testFile}:${lineNumber} - it()`);
+        }
+    }
+};
+
 const allTestFiles = getTestFiles('.');
 
-console.log(allTestFiles);
-
+allTestFiles.forEach(file => {
+    findLargeTests(file);
+});
 
 // TODO:
-// 1. find all test files
 // 2. for each test file, split into lines
 // start counting lines once a 'it()' is found, until next 'it(), describe(), beforeEach(), afterEach(), EOF, etc)
-// keep track of file / describe / it names?
+// keep track of file / describe / it names? or just filename & line number?
 // sort by most lines? allow argument to find tests over a certain number of lines?
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,15 @@
-// TODO: test index file
+hmmmmm
+
+
+it('is a test!');
+
+
+       it(`is another test!!!`);
+
+
+its(not a test)
+
+ITS(NOT A TEST)
+
+
+hiya
